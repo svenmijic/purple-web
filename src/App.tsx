428afc9520b84bb9c1b3d7bc0c7f3fc01cce098c
Sven Mijic <svenmijic@gmail.com>
2023-12-04 22:37:32 +0100

    Switch to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 export const App = () => {
     return (
-        <div className="App">
-            <i className="pi pi-cog pi-spin"></i>
-            <h2>Under construction...</h2>
+        <div>
+            <h1>
+                <i className="pi pi-spin pi-cog" style={{ fontSize: "6rem" }}></i>
+            </h1>
+            <h1>coming soon...</h1>
         </div>
     );
 };
